54d57fc8025d47053d25726c6b885bca78ba1971
Joan148 <i202112603@cibertec.edu.pe>
2023-09-24 08:10:13 -0500

    Clases
    Adicion de las clases Alumn y Contestant

---
+++ WololoPrueba/Models/Alumn.cs
@@ -0,0 +1,16 @@
+﻿namespace WololoPrueba.Models
+
+public class Alumn
+{
+    public Alumn()
+    {
+        public int AlumnId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Dni { get; set; }
+        public Date BirthDate { get; set; }
+        public string Email { get; set; }
+
+}
+}
+
---
+++ WololoPrueba/Models/Contestant.cs
@@ -0,0 +1,21 @@
+﻿namespace WololoPrueba.Models
+    //Esta clase Concursantes en inglés sería como el registro del concurso, se guargan alumno, centro educativo,civilizacion elegida.
+public class Constestant
+{
+    public Constestant()
+    {
+
+        public int ContestantId { get; set; }
+        public int AlumnId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int CentroEduId { get; set; }
+        public int CivId { get; set; }    
+        public string Result { get; set; } //Nota, resultado, calificación del ensayo
+        
+
+
+
+}
+}
+
